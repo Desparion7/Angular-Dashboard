5492347a8170401f2c238aecb08b250a4538c28b
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-09-02 14:43:00 +0200

    move logic for add new category to service

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { CategoriesService } from '../services/categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -8,39 +8,12 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
-  constructor(private afs: AngularFirestore) {}
+  constructor(private categoriesService: CategoriesService) {}
 
   onSubmit(formData: NgForm) {
     const categoryData = {
       category: formData.value.category,
     };
-    const subCategoryData = {
-      subCategory: 'subCategory1',
-    };
-    this.afs
-      .collection('categories')
-      .add(categoryData)
-      .then((docRef) => {
-        console.log(docRef);
-        this.afs
-          .collection('categories')
-          .doc(docRef.id)
-          .collection('subcategories')
-          .add(subCategoryData)
-          .then((docRef1) => {
-            console.log(docRef1);
-            this.afs
-              .collection('categories')
-              .doc(docRef.id)
-              .collection('subcategories')
-              .doc(docRef1.id)
-              .collection('subsubcategories')
-              .add(subCategoryData)
-              .then((docRef2) => {
-                console.log('Second Level Subcategory Saved Successfully');
-              });
-          });
-      })
-      .catch((err) => console.log(err));
+    this.categoriesService.saveData(categoryData);
   }
 }
